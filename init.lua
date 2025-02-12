59f245ba8e08ec153f6e09cf5509af8baa8a8371
Birk Nergaard <birknergard@gmail.com>
2025-02-12 12:59:35 +0100

    Updated config for windows.

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = 'L'
-vim.g.maplocalleader = 'l'
+vim.g.mapleader = 'M'
+vim.g.maplocalleader = 'M'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -105,7 +105,18 @@ vim.opt.shellxquote = ''
 -- vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
+vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Update numbers dynamically when moving the cursor
+vim.api.nvim_create_autocmd('CursorMoved', {
+  pattern = '*',
+  callback = function()
+    vim.opt.relativenumber = true
+    vim.opt.number = true
+  end,
+})
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -122,7 +133,7 @@ vim.schedule(function()
 end)
 
 -- Enable break indent
-vim.opt.breakindent = true
+vim.opt.breakindent = false
 
 -- Save undo history
 vim.opt.undofile = true
@@ -158,6 +169,13 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
+
+vim.opt.expandtab = false -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Number of spaces per indentation level
+vim.opt.tabstop = 4 -- Number of spaces per tab
+vim.opt.softtabstop = 4 -- Spaces when pressing <Tab>
+vim.opt.autoindent = false -- Maintain indentation on new lines
+vim.opt.smartindent = false -- Auto-indent based on syntax
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -852,12 +870,17 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'ayu-dark'
+
+      -- Transparent background
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', blend = 10 })
       vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none', blend = 10 })
       vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none', blend = 10 })
       vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none', blend = 10 })
+
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.api.nvim_set_hl(0, 'LineNr', { fg = '#FF5757', bg = 'none' })
+      vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#FFA98A', bold = true })
     end,
   },
 
